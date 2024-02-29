9aab28550261f004267a949a428ee1d3ff0551ae
IssOT <136982393+IssOT@users.noreply.github.com>
2024-02-29 17:10:16 +0700

    Update vercel.json

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT|| 3000;
-
-var lamp1Status = "off";
-
-  /*get status*/
-app.get('/lamp1Status', (req, res) => {
-    res.json({message:lamp1Status})
-  });
-
-  /*turn on*/
-app.get('/turnOnLamp1',(req,res)=>{
-  lamp1Status = "on"
-    res.json({lamp1:lamp1Status})
-});
-
-  /*turn off*/
-app.get('/turnOffLamp1',(req,res)=>{
-  lamp1Status = "off"
-    res.json({lamp1:lamp1Status})
-});
-
-app.listen(port, () => {
-  console.log(`cli-nodejs-api listening at http://localhost:${port}`)
-});
+{
+  "name": "be-esp32-control",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
